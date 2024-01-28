05021366430c4a25c091c2e493350a16120ba29b
Otavio H. P. Valadares <otaviopvaladares@gmail.com>
2024-01-28 17:59:13 -0300

    basic instruction set

--- main.go
+++ main.go
@@ -44,17 +44,19 @@ type Instruction struct {
 	X     [2]byte
 	Y     [2]byte
 	N     [2]byte
-	NN    [2]byte
+	NN    [2]byte // KK
 	NNN   [2]byte
 }
 
 func main() {
-	//var stack [32]uint16   // The stack offers a max depth of 32 with 2 bytes per stack frame
-	//var stackFrame int     // current stack frame. Starts at -1 and is set to 0 on first use
-	//var I uint16 // represents Index register
-	//var registers [16]byte // represents the 16 1-byte registers
-	var pc uint16         // Program counter, set it to the initial memory offset
-	var memory [4096]byte //4kb internal memory
+	var stack [32]uint16   // The stack offers a max depth of 32 with 2 bytes per stack frame
+	var stackFrame int     // current stack frame. Starts at -1 and is set to 0 on first use
+	var I uint16           // represents Index register
+	var registers [16]byte // represents the 16 1-byte registers
+	var pc uint16          // Program counter, set it to the initial memory offset
+	var memory [4096]byte  //4kb internal memory
+
+	stackFrame = -1
 
 	programData, err := os.ReadFile("IBMLogo.ch8")
 	if err != nil {
@@ -74,25 +76,9 @@ func main() {
 		memory[FONT_OFFSET+i] = FONT_SET[i]
 	}
 
-	// Place program at memory
 	for i := range programData {
 		memory[MEMORY_OFFSET+i] = programData[i]
 	}
-
-	// instructions := []*Instruction{}
-
-	// for i := 0; i < len(programData); i += 12 {
-	// 	instr := new(Instruction)
-
-	// 	copy(instr.INSTR[:], programData[i:i+2])
-	// 	copy(instr.X[:], programData[i+2:i+4])
-	// 	copy(instr.Y[:], programData[i+4:i+6])
-	// 	copy(instr.N[:], programData[i+6:i+8])
-	// 	copy(instr.NN[:], programData[i+8:i+10])
-	// 	copy(instr.NNN[:], programData[i+10:i+12])
-
-	// 	instructions = append(instructions, instr)
-	// }
 
 	// fmt.Printf("First instruction: %02x", instructions[0].INSTR)
 
@@ -129,37 +115,60 @@ func main() {
 			case 0x0E:
 				fmt.Print("clear scren\n")
 			case 0xEE:
+				pc = stack[stackFrame]
+				stackFrame--
 				fmt.Print("set stack pointer to the top\n")
 			default:
 				fmt.Printf("Unknown instruction. INSTR: %02x Y: %02x\n", instr, Y)
 			}
 		case 0x1:
+			pc = NNN
 			fmt.Printf("JUMP to NNN: %02x\n", NNN)
 			fmt.Printf("SET PROGRAM COUNTER to NNN: %02x\n", NNN)
 		case 0x2:
+			stackFrame++
+			stack[stackFrame] = pc
+			pc = NNN
 			fmt.Printf("CALL subroutine at NNN: %02x\n", NNN)
 			fmt.Printf("Increment stack pointer\n")
 			fmt.Printf("Put PC at top of the stack. PC: %d\n", pc)
 			fmt.Printf("SET PC to NNN. PC: %d, NNN: %02x\n", pc, NNN)
 		case 0x3:
+			if registers[X] == NN {
+				pc += 2
+			}
+
 			fmt.Printf("Skip next instruction if Vx = kk.\n")
 		case 0x4:
+			if registers[X] != NN {
+				pc += 2
+			}
 			fmt.Printf("Skip next instruction if Vx != kk.\n")
 		case 0x5:
+			if registers[X] == registers[Y] {
+				pc += 2
+			}
 			fmt.Printf("Skip next instruction if Vx = Vy.\n")
 		case 0x6:
+			registers[X] = NN
+
 			fmt.Printf("Set Vx = KK\n")
 		case 0x7:
+			registers[X] = NN + registers[X]
 			fmt.Printf("Set Vx = Vx + KK\n")
 		case 0x8:
 			switch N {
 			case 0x0:
+				registers[X] = registers[Y]
 				fmt.Printf("Set Vx = Vy\n")
 			case 0x1:
+				registers[X] = registers[X] | registers[Y]
 				fmt.Printf("Set Vx = Vx OR Vy\n")
 			case 0x2:
+				registers[X] = registers[X] & registers[Y]
 				fmt.Printf("Set Vx = Vx AND Vy\n")
 			case 0x3:
+				registers[X] = registers[X] ^ registers[Y]
 				fmt.Printf("Set Vx = Vx XOR Vy\n")
 			case 0x4:
 				fmt.Printf("Set Vx = Vx + Vy, set VF = carry\n")
@@ -173,8 +182,12 @@ func main() {
 				fmt.Print("Set Vx = Vx SHL 1.\n")
 			}
 		case 0x9:
+			if registers[X] != registers[Y] {
+				pc += 2 // SKIP INSTRUCTION (wrap on function)
+			}
 			fmt.Printf("Skip next instruction if Vx != Vy.\n")
 		case 0xA:
+			I = NNN
 			fmt.Printf("Set I = nnn.")
 		case 0xB:
 			fmt.Printf("Jump to location nnn + V0.\n")
@@ -188,6 +201,8 @@ func main() {
 				fmt.Printf("Skip next instruction if key with the value of Vx is pressed.\n")
 			case 0xA1:
 				fmt.Printf("Skip next instruction if key with the value of Vx is not pressed.\n")
+			default:
+				fmt.Printf("Unknown Instruction\n")
 			}
 		case 0xF:
 			fmt.Printf("Timer instruction.\n")
